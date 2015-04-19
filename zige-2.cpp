35b8d0f9f14f4b7195458e47b03d6e8c857b3e3d
Gaoce <gaocegege@hotmail.com>
2015-04-19 14:39:39 +0800

    update

--- zige-2.cpp
+++ zige-2.cpp
@@ -35,16 +35,44 @@ int dp(const string &s)
 		dpCount[i][i] = 1;
 	}
 
-	for(int i=1; i<sz; i++){
-        int tmp = 0;
-        for(int j=0; j+i<sz; j++){
-            if(s[j] == s[j+i]){
-                dpCount[j][j+i] = (getCount(dpCount, j + 1, j + i - 1, sz) + 1) % 100007;
-            }
-        }
-    }
+	for(int i = 1; i < sz; i++)
+	{
+		int tmp = 0;
+		for(int j = 0; j + i < sz; j++)
+		{
+			if(s[j] == s[j + i])
+			{
+				dpCount[j][j + i] = (getCount(dpCount, j + 1, j + i - 1, sz) + 1) % 100007;
+			}
+		}
+	}
 
 	return getCount(dpCount, 0, sz - 1, sz);
+}
+
+// wrong
+int dp2(const string &s)
+{
+	int sz = s.size();
+	vector<vector<int> > dpCount(sz, vector<int>(sz, 0));
+	for (int i = 0; i < sz; i++)
+	{
+		dpCount[i][i] = 1;
+	}
+
+	for(int i = 1; i < sz; i++)
+	{
+		int tmp = 0;
+		for(int j = 0; j + i < sz; j++)
+		{
+			if(s[j] == s[j + i])
+				dpCount[j][j + i] = (2 * dpCount[j + 1][j + i - 1] + 1) % 100007;
+			else
+				dpCount[j][j + i] = (dpCount[j + 1][j + i - 1] + dpCount[j + 1][j + i] + dpCount[j][j + i - 1]) % 100007;
+		}
+	}
+
+	return dpCount[0][sz - 1];
 }
 
 int main(int argc, char const *argv[])
